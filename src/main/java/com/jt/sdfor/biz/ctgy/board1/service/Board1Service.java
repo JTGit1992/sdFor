980816a39beb539cd7c14f18ed9ed188a512f8ed
kurt92 <chcch1234@gmail.com>
2024-06-25 17:17:33 +0900

    feat : 샘플코드 작성 # 작업내용 : 샘플코드 작성, 불필요한 코드정의 제거

--- src/main/java/com/jt/sdfor/biz/ctgy/board1/service/Board1Service.java
+++ src/main/java/com/jt/sdfor/biz/ctgy/board1/service/Board1Service.java
@@ -3,7 +3,10 @@ package com.jt.sdfor.biz.ctgy.board1.service;
 
 import com.jt.sdfor.biz.ctgy.board1.dto.Board1Dto;
 import com.jt.sdfor.biz.ctgy.board1.repository.Board1QueryDslRepository;
+import com.jt.sdfor.biz.domain.board.Board;
 import com.jt.sdfor.biz.domain.board.BoardRepository;
+import com.jt.sdfor.framework.core.exception.BaseException;
+import com.jt.sdfor.framework.core.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +29,15 @@ public class Board1Service {
         result = board1QueryDslRepository.sample1();
 
         return result;
+    }
+
+    @Transactional
+    public List<Board> sample2() {
+        return boardRepository.findAll();
+    }
+
+    public Board sample3() {
+        return boardRepository.findById(9999L).orElseThrow(() -> new BaseException(ErrorCode.DATA_NOT_FOUND));
     }
 
 }
