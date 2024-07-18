12f5892cbf953d7f8611c621979f63a6e528c707
kurt92 <chcch1234@gmail.com>
2024-07-18 18:04:53 +0900

    feat : 메뉴트리, cors설정 # 작업내용 : 메뉴트리 디비조회 추가. cors설정 추가

---
+++ src/main/java/com/jt/sdfor/framework/support/domain/MenuMng.java
@@ -0,0 +1,37 @@
+package com.jt.sdfor.framework.support.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Entity
+@Table(name = "com_menu_mng")
+public class MenuMng {
+
+    @Id
+    @Column(name = "menu_id")
+    @Comment("메뉴ID")
+    private String menuId;
+
+    @Column(name = "menu_dc")
+    @Comment("메뉴 설명")
+    private String menuDc;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent")
+    private MenuMng parent;
+
+    @Column(name = "list_order")
+    @Comment("메뉴정렬순서")
+    private Integer listOrder;
+
+    @Column(name = "menu_url")
+    @Comment("메뉴URL")
+    private String menuUrl;
+
+
+
+}
---
+++ src/main/java/com/jt/sdfor/framework/support/menu/service/MenuMngService.java
@@ -0,0 +1,13 @@
+package com.jt.sdfor.framework.support.menu.service;
+
+
+import com.jt.sdfor.framework.support.domain.MenuMng;
+
+import java.util.List;
+
+public interface MenuMngService {
+
+    List<MenuMng> findMenuTree();
+
+
+}
